f66146eaa46244819c69b8f444d8873b5e6af3f8
Dmitrii Mudrechenko <D.Mudrechenko@gmail.com>
2014-08-19 15:56:25 -0400

    Chaching is now for getMenu, getHours and DH list

--- www/js/diningEvents.js
+++ www/js/diningEvents.js
@@ -23,51 +23,65 @@ $(document).on('click', '#get-time', function(e){
       
 function getMenu(meal,diningHall,date){
           //Takes meal, dining hall name and date. Shows menu as popup
-          var text = '';
-          $.getJSON("https://www.smith.edu/its/api/diningservices/dininghallmeal/"+meal+"/"+diningHall+"/"+date, function(data){
-                   if (data.length != 0) {
-                              $.each(data, function (index, value) {
-                                        text = text.concat(value.item_text + '\n');
-                              });
-                              text = text.replace('</br>','');
-                              navigator.notification.alert(
-                                        text,  // message
-                                        alertDismissed,         // callback
-                                        'Menu',            // title
-                                        'Ok'                  // buttonName
-                              );
-                    }
-                    else{
-                        window.plugins.toast.show('No menu available', 'short', 'bottom');   //notify user   
-                    }
-          });
+          if (window.sessionStorage.getItem(meal+diningHall+date) != null) {//DH and meal pair to distinguish menus
+                    //alert("cached");
+                    showMessage(window.sessionStorage.getItem(meal+diningHall+date));                  
+          }
+          else {
+                    var text = '';
+                    $.getJSON("https://www.smith.edu/its/api/diningservices/dininghallmeal/"+meal+"/"+diningHall+"/"+date, function(data){
+                             if (data.length != 0) {
+                                        $.each(data, function (index, value) {
+                                                  text = text.concat(value.item_text + '\n');
+                                        });
+                                        text = text.replace('</br>','');
+                                        window.sessionStorage.setItem(meal+diningHall+date,text);
+                                        showMessage(text);
+                              }
+                              else{
+                                  window.plugins.toast.show('No menu available', 'short', 'bottom');   //notify user   
+                              }
+                    });
+          }
 }
-      
+
+function getHours(meal,diningHall, day){
+          if (window.sessionStorage.getItem(meal+diningHall+day) != null) {//meal + dh pair to distinguish menus
+                    //alert("cached");
+                    showMessage(window.sessionStorage.getItem(meal+diningHall+day));                  
+          }
+          else {
+                    var text = ''; //initialize ouput message
+                    $.getJSON("https://www.smith.edu/its/api/diningservices/dininghallhours/"+diningHall, function(data){
+                              if (data.length != 0){
+                                        $.each(data, function (index, value) {//chek for meal and day in each entry
+                                                  if (value.meal == meal && $.inArray(day, value.days.split(',')) != -1) {
+                                                            text = value.information;
+                                                            text = text.replace(/<(?:.|\n)*?>/gm, ' ');//stripping html tags
+                                                            window.sessionStorage.setItem(meal+diningHall+day,text);
+                                                            showMessage(text);
+                                                  }
+                                        });
+                              }
+                              else{
+                                  window.plugins.toast.show('No data available', 'short', 'bottom');
+                              }
+                  });
+          }
+}
+
 function alertDismissed() {
     // do nothing
-}      
-  
-function getHours(meal,diningHall, day){
-          var text = ''; //initialize ouput message
-          $.getJSON("https://www.smith.edu/its/api/diningservices/dininghallhours/"+diningHall, function(data){
-                    if (data.length != 0){
-                              $.each(data, function (index, value) {//chek for meal and day in each entry
-                                        if (value.meal == meal && $.inArray(day, value.days.split(',')) != -1) {
-                                                  text = value.information;
-                                                  text =text.replace(/<(?:.|\n)*?>/gm, ' ');//stripping html tags
-                                                  navigator.notification.alert(
-                                                  text,  // message
-                                                  alertDismissed,         // callback
-                                                  'Hours',            // title
-                                                  'Ok'                  // buttonName
-                                                  );       
-                                        }
-                              });
-                    }
-                    else{
-                        window.plugins.toast.show('No data', 'short', 'bottom');
-                    }
-        });      
+}
+
+function showMessage(message){
+          //notifies user via alert-like popup with passed message
+          navigator.notification.alert(
+                                       message,  // message to show
+                                       alertDismissed,         // callback
+                                       'Menu',            // title
+                                       'Ok'                  // buttonName
+          );
 }
 
 function getDayNumber(date) {
--- www/js/onDiningStartup.js
+++ www/js/onDiningStartup.js
@@ -17,6 +17,7 @@ function setDate() {
             var month = date.getMonth() + 1;
             var year = date.getFullYear();
             
+            
             if (month < 10) month = "0" + month;
             if (day < 10) day = "0" + day;
             
@@ -27,18 +28,20 @@ function setDate() {
 function getDiningHalls(){
             //inflates control group with values from JSON
             //check if we have values in current session
-            if (window.sessionStorage.length > 0){
-                        //iterate through current session's storage 
-                        for (var i = 0; i < window.sessionStorage.length; i++){
+            if (window.sessionStorage.getItem('dining-halls') != null ) {
+                        //values stored as string, we need to get array from it
+                        var storedHalls = JSON.parse(window.sessionStorage.getItem('dining-halls'));
+                        //iterate through array
+                        for (var i = 0; i < storedHalls.length; i++){
                                     //append radio buttons
                                     $('#dh-choice')
                                     .controlgroup("container")
-                                    .append(sessionStorage.getItem(sessionStorage.key(i)));
+                                    .append(storedHalls[i]);
                         }
-                        $("input[type='radio']:first").attr("checked", "checked");//!!Selects the very first radio on the page
-                        $("#dh-choice").trigger("create");//make sure control group is displayed updated     
+                        updateList();//make sure displayed properly  
             }
             else {//if storage is empty we get values from JSON, append them to CG and store in session storage
+                        var hallStorage = [];
                         $.getJSON("https://www.smith.edu/its/api/diningservices/dininghalls", function(data){
                           $.each(data, function (index, value) {
                                     var $new = ('<input type="radio" name="dhall-choice" id="'+value.dining_hall_name
@@ -48,15 +51,20 @@ function getDiningHalls(){
                             $('#dh-choice')
                             .controlgroup("container")
                             .append($new); //append radio button with DH id/name/value
-                            window.sessionStorage.setItem(value.dining_hall_name,$new);
+                            hallStorage.push($new);
                         });
-                        //select first radio button
-                        $("input[type='radio']:first").attr("checked", "checked");//!!Selects the very first radio on the page
-                        $("#dh-choice").trigger("create");//make sure control group is displayed updated
+                        window.sessionStorage.setItem('dining-halls',JSON.stringify(hallStorage));
+                        updateList();
                         });
             }
 }
-          
+   
+function updateList(){
+            //updates dh list
+            $("input[type='radio']:first").attr("checked", "checked");//!!Selects the very first radio on the page
+            $("#dh-choice").trigger("create");//make sure control group is displayed updated  
+}
+
 function setHeight(){
             var div= $('#dining-halls');
             div.css("max-height", ($(window).height() - div.height())/2.5 + 'px'); //set dininghall scroll height to 1/3 to fit the screen
